a48e181aa06729ef6e1f7c64d92b4f06907cf588
vikassingh135 <vikassingh1352003@gmail.com>
2023-06-17 14:10:05 +0530

    done using array

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 let id = 1;
 
+const users = [
+  
+];
+
+const displayUsers = () => {
+  const employeesList = document.getElementById("employees-list");
+  employeesList.innerHTML = "";
+
+  users.forEach((user, index) => {
+    employeesList.innerHTML += `
+        <div class="employee">
+        <div class="details">
+        <span>${user.id}.</span>
+        <span>Name: ${user.name}</span>
+        <span>Profession: ${user.profession}</span>
+        <span>Age: ${user.age}</span>
+        </div>
+            <button onclick="deleteUser(${index})">Delete user</button>
+        </div>
+    `;
+  });
+};
+
 const addUser = () => {
   const messageDiv = document.getElementById("add-employee-message");
   messageDiv.innerHTML = "";
@@ -11,26 +34,23 @@ const addUser = () => {
       "Error : Please Make sure All the fields are filled before adding in an employee !";
     messageDiv.style.color = "#FF4343";
   } else {
-    const employeesList = document.getElementById("employees-list");
-    employeesList.innerHTML += `
-        <div class="employee">
-        <div class="details">
-        <span>${id}.</span>
-        <span>Name: ${name}</span>
-        <span>Profession: ${profession}</span>
-        <span>Age: ${age}</span>
-        </div>
-            <button onclick="deleteUser(event)">Delete user</button>
-        </div>
-    `;
+    users.push({
+      id: id,
+      name: name,
+      profession: profession,
+      age: age,
+    });
+    displayUsers();
     messageDiv.innerText = `Success : Employee Added!`;
     messageDiv.style.color = "#43FF78";
     id++;
   }
 };
 
-const deleteUser = (event) => {
-    event.target.parentNode.remove();
-}
+const deleteUser = (index) => {
+  users.splice(index, 1);
+  displayUsers();
+};
 
 
+displayUsers();
